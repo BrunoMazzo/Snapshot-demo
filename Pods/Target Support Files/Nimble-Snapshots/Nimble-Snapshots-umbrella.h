c232e31d638d029c40f856d2271b9ee5571794ed
Bruno Mazzo <mazzo.bruno@gmail.com>
2016-11-23 23:43:57 -0200

    Initial commit

---
+++ Pods/Target Support Files/Nimble-Snapshots/Nimble-Snapshots-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "NBSMockedApplication.h"
+
+FOUNDATION_EXPORT double Nimble_SnapshotsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Nimble_SnapshotsVersionString[];
+
---
+++ SnapshotTests/NimbleLikeTests.swift
@@ -0,0 +1,37 @@
+//
+//  NimbleLikeTests.swift
+//  Snapshot
+//
+//  Created by Bruno Mazzo on 11/23/16.
+//  Copyright © 2016 Bruno Mazzo. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+import Nimble_Snapshots
+
+@testable import Snapshot
+
+class NimbleLikeTests: QuickSpec {
+    
+    override func spec() {
+        describe("Nimble-Snapshot") {
+            var viewController: UIViewController!
+            
+            beforeEach {
+                setNimbleTolerance(0)
+                setNimbleTestFolder("tests")
+                viewController = UIStoryboard(name: "Main", bundle: Bundle(for: ViewController.self)).instantiateInitialViewController()
+            }
+            
+            it("should have spected snapshot") {
+                expect(viewController) == snapshot("ViewController")
+            }
+            
+            it("should have spected snapshot for sizes") {
+                expect(viewController) == snapshot("ViewController", sizes: ["Big": CGSize(width: 500, height: 700), "Small": CGSize(width: 320, height: 400)])
+            }
+        }
+    }
+}
